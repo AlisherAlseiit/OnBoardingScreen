7f56c6c432207c85c2dc3a739bdc5d7cfe6b6b46
Alisher <Alisher>
2021-10-13 16:20:59 +0600

    Initial Commit

---
+++ AnimatedIndicator/AnimatedIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedIndicatorApp.swift
+//  AnimatedIndicator
+//
+//  Created by Алишер Алсейт on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
